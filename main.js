64aeb153452ff9d5947e07d043ea8aaa18403ab7
parthokr <partho.rajvor2016@gmail.com>
2022-05-26 01:19:10 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use((err, req, res, next) => {
+    // This check makes sure this is a JSON parsing issue, but it might be
+    // coming from any middleware, not just body-parser:
+
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+        console.error(err);
+        return res.sendStatus(400); // Bad request
+    }
+
+    next();
+});
+
+const userRoute = require('./routes/user.route')
+const materialRoute = require('./routes/material.route')
+
+app.use('/api/users', userRoute)
+app.use('/api/materials', materialRoute)
+
+app.use((err, req, res, next) => {
+    res.status(err.code).send({error: err.message})
+})
+
+
+app.listen('8000', () => {
+    console.log('Server running at port 8000');
+})
