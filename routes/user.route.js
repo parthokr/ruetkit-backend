f23fe6b2d5306bb21c5e234f0f2d1586ae3c9429
parthokr <partho.rajvor2016@gmail.com>
2022-05-27 06:57:53 +0600

    Imported userController and corresponding routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,218 +1,44 @@
 require('dotenv').config()
 const express = require('express')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 const userRoute = express.Router()
 
-const { PrismaClient } = require('@prisma/client')
-const { generateAccessToken, generateRefreshToken } = require('../services/generateToken')
+// import all middlewares
 const auth = require('../middlewares/auth')
 const requireStaff = require('../middlewares/requireStaff')
 
-const prisma = new PrismaClient()
+const userController = require('../controllers/userController')
 
+userRoute.post('/signup', userController.signUp)
 
-class RuetkitError extends Error {
-    constructor(code = 500, message = 'Something went wrong') {
-        super(code)
-        this.code = code
-        this.message = message
-    }
-}
+userRoute.post('/signin', userController.signIn)
 
-userRoute.post('/signup', async (req, res, next) => {
-    const { ruet_id, fullname, email, password } = req.body
+// verify user
+userRoute.post('/:id/verify', userController.verify)
 
+// resend verification code
+// TODO make it patch
+userRoute.patch('/:id/verify', userController.resendVerification)
 
-    let errors = ['ruet_id', 'fullname', 'email', 'password'].filter((field) => {
-        return req.body[field] === undefined || req.body[field] === ''
-    })
-    errors = errors.map((field, _index) => {
-        return { field, detail: `${field} is required` }
-    })
-
-    if (errors.length !== 0) {
-        return next(new RuetkitError(401, errors))
-    }
-    try {
-        const user = await prisma.user.create({
-            data: {
-                ruet_id,
-                fullname,
-                email,
-                password: await bcrypt.hash(password, 10),
-                status: 'not_verified',
-                role: 'user',
-                code: {
-                    create: {
-                        code: Math.random().toString(36).slice(2, 8).toUpperCase()
-                    }
-                }
-            }
-        })
-        res.send({ id: user.id })
-    } catch (e) {
-        console.log(e)
-        if (e.code === 'P2002') {
-            return next(new RuetkitError(401, { field: e.meta.target[0], detail: `${e.meta.target[0]} is already used` }))
-        }
-        return next(new RuetkitError())
-    }
-})
-
-userRoute.post('/signin', async (req, res, next) => {
-    const { ruet_id, password } = req.body
-    let errors = ['ruet_id', 'password'].filter((field) => {
-        return req.body[field] === undefined || req.body[field] === ''
-    })
-    errors = errors.map((field, _index) => {
-        return { field, detail: `${field} is required` }
-    })
-
-    if (errors.length !== 0) {
-        return next(new RuetkitError(401, errors))
-    }
-
-    try {
-        const user = await prisma.user.findUnique({
-            where: { ruet_id },
-            select: {
-                id: true,
-                fullname: true,
-                password: true,
-                status: true,
-                role: true
-            }
-        })
-        if (user === null) {
-            return next(new RuetkitError(401, {detail: 'This user doesn\'t exist'}))
-        }
-        if (user.status !== 'verified') {
-            return next(new RuetkitError(403, {detail: 'This user is not verified yet'}))
-        }
-        if (await bcrypt.compare(password, user.password)) {
-            const accessToken = await generateAccessToken({ id: user.id, fullname: user.fullname, role: user.role })
-            const refreshToken = await generateRefreshToken({ id: user.id, fullname: user.fullname,role: user.role })
-            res.status(200).send({ accessToken, refreshToken, fullname: user.fullname })
-        } else {
-            return next(new RuetkitError(401, {detail: 'Invalid credentials provided'}))
-        }
-    } catch (e) {
-        console.log(e)
-
-        return next(new RuetkitError())
-    }
-})
-
-userRoute.post('/:id/verify', async (req, res, next) => {
-    const userId = Number(req.params.id)
-    const { code } = req.body
-    if (isNaN(userId)) return next(new RuetkitError(400, { field: 'code', detail: 'Invalid user id provided' }))
-    if (code === undefined || code === '') return next(new RuetkitError(400, { field: 'code', detail: 'Code is required' }))
-    try {
-        const user = await prisma.user.findUnique({
-            where: { id: userId },
-            select: { id: true, status: true, code: true, role: true }
-        })
-        if (user === null) {
-            res.sendStatus(404)
-        }
-        if (user.status === 'verified') {
-            return next(new RuetkitError(403, { detail: 'This user is already verified' }))
-        }
-        if (user.code.code === code) {
-            prisma.user.update({
-                where: { id: userId },
-                data: {
-                    status: 'verified'
-                }
-            }).then(async user => {
-                const accessToken = await generateAccessToken({ id: user.id, fullname: user.fullname, role: user.role })
-                const refreshToken = await generateRefreshToken({ id: user.id, fullname: user.fullname, role: user.role })
-                res.status(200).send({ accessToken, refreshToken })
-            })
-        }
-        else {
-            return next(new RuetkitError(400, { field: 'code', detail: 'Invalid code provided' }))
-        }
-    } catch (e) {
-        console.log(e)
-        return next(new RuetkitError())
-    }
-})
-
-userRoute.get('/:id/verify', async (req, res, next) => {
-    const userId = Number(req.params.id)
-    if (isNaN(userId)) return next(new RuetkitError(400, { field: 'code', detail: 'Invalid user id provided' }))
-    try {
-        const user = await prisma.user.findUnique({
-            where: {
-                id: userId
-            },
-            select: {
-                status: true
-            }
-        })
-        if (user === null) 
-            return next(new RuetkitError(404, { detail: 'This user was not found' }))
-
-        if (user.status === 'verified') return next(new RuetkitError(403, {detail: 'This user is already verified'}))
-
-        await prisma.user.update({
-            where: { id: userId },
-            data: {
-                code: {
-                    update: {
-                        code: Math.random().toString(36).slice(2, 8).toUpperCase()
-                    }
-                }
-            }
-        }).then(async _user => {
-            res.sendStatus(200)
-        })
-    } catch (e) {
-        return next(new RuetkitError())
-    }
-
-})
-
-userRoute.get('/', [auth, requireStaff], async (req, res, next) => {
-    const users = await prisma.user.findMany({
-        select: {
-            id: true,
-            fullname: true,
-            email: true,
-            role: true,
-            status: true
-        }
-    })
-
-    res.send(users)
-})
+// list users (experimental endpoint)
+userRoute.get('/', [auth, requireStaff], userController.listUsers)
 
 // verify existing access token
-userRoute.post('/token', [auth], async (req, res, next) => {
-    res.status(200).send({fullname: req.user.fullname})
-})
+userRoute.post('/token', [auth], userController.verifyToken)
 
 // refresh access token using refreshToken
-userRoute.post('/token/refresh', async (req, res, next) => {
-    const { refreshToken } = req.body
-    // console.log(refreshToken)
-    if (refreshToken === null || refreshToken === undefined || refreshToken === '') {
-        return next(new RuetkitError(400, { field: 'refreshToken', detail: 'Refresh token is required' }))
-    }
+userRoute.post('/token/refresh', userController.refreshToken)
 
-    try {
-        const user = jwt.verify(refreshToken, process.env.JWT_PRIVATE_KEY)
-        const accessToken = await generateAccessToken({ id: user.id, role: user.role })
-        res.status(200).send({ accessToken })
-    } catch (e) {
-        console.log(e)
-        res.sendStatus(401)
-        // return next(new RuetkitError())
-    }
-})
+// send password reset verification code
+userRoute.get('/:ruetId/password/reset', userController.sendPasswordResetVerification)
+
+// resend password reset verification code
+userRoute.patch('/:id/password/reset', userController.resendPasswordResetVerification)
+
+// verify password reset verification code (ping)
+userRoute.post('/:id/password/reset/verify', userController.verifyPasswordResetVerification)
+
+// reset password
+userRoute.post('/:id/password/reset', userController.resetPassword)
 
 
 module.exports = userRoute
