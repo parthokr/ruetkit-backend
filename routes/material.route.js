0d88434e791b38de7d13eb8ce90ef43821c01b1d
parthokr <partho.rajvor2016@gmail.com>
2022-05-29 04:40:14 +0600

    Added materialRoute for specific material

--- routes/material.route.js
+++ routes/material.route.js
@@ -6,6 +6,9 @@ const materialController = require('../controllers/materialController')
 
 materialRoute.get('/', [auth], materialController.listMaterials)
 
+materialRoute.get('/:materialId', [auth], materialController.getMaterial)
+
+
 materialRoute.post('/', [auth], materialController.createMaterialMeta)
 
 module.exports = materialRoute
