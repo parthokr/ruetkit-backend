141fc0f002426af79e7f06d890dee2cc3a825248
parthokr <partho.rajvor2016@gmail.com>
2022-05-30 00:06:16 +0600

    Fixed variable

--- routes/course.route.js
+++ routes/course.route.js
@@ -3,9 +3,9 @@ const auth = require('../middlewares/auth')
 const requireStaff = require('../middlewares/requireStaff')
 const courseRoute = express.Router()
 
-const courseRoute = require('../controllers/departmentController')
+const courseController = require('../controllers/courseController')
 
-courseRoute.get('/', [auth, requireStaff], departmentController.listAllDepartments)
-courseRoute.post('/', [auth, requireStaff], departmentController.createDeapartment)
+courseRoute.get('/', [auth, requireStaff], courseController.listCourse)
+courseRoute.post('/', [auth, requireStaff], courseController.createCourse)
 
-module.exports = departmentRoute
+module.exports = courseRoute
