e55540adc8f7ac5d534e82006f7b3b3763adf4f1
parthokr <partho.rajvor2016@gmail.com>
2022-06-01 15:00:08 +0600

    Returning departments ordered by acronym

--- controllers/departmentController.js
+++ controllers/departmentController.js
@@ -4,7 +4,11 @@ const prisma = new PrismaClient()
 
 exports.listAllDepartments = async (req, res, next) => {
     try {
-        const departments = await prisma.department.findMany()
+        const departments = await prisma.department.findMany({
+            orderBy: {
+                acronym: 'asc'
+            }
+        })
         res.send(departments)
     } catch(err) {
         console.log(err)
@@ -36,3 +40,35 @@ exports.createDeapartment = async (req, res, next) => {
     }
 }
 
+exports.listCoursesOfADepartment = async (req, res, next) => {
+    const {departmentId} = req.params
+
+    if (departmentId === null || departmentId === '' || departmentId === undefined) {
+        return next(new RuetkitError(400, {detail: 'Please specify a department'}))
+    }
+
+    try {
+        const courseOfADepartment = await prisma.department.findUnique({
+            select: {
+                course: {
+                    select: {
+                        id: true,
+                        code: true,
+                        title: true
+                    }
+                }
+            },
+            where: {
+                id: Number(departmentId)
+            }
+        })
+        if (courseOfADepartment === null) {
+            return next(new RuetkitError(404, {detail: 'Department was not found'}))
+        }
+        res.status(200).send(courseOfADepartment.course)
+    } catch (err) {
+        console.log(err)
+    } finally {
+        prisma.$disconnect()
+    }
+}
