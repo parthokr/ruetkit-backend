4266a7ad4d6c0a592211fb95338d9b014068c703
parthokr <partho.rajvor2016@gmail.com>
2022-06-11 15:07:12 +0600

    Added department admin routes

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -4,9 +4,15 @@ const requireStaffOrAdmin = require('../middlewares/requireStaffOrAdmin')
 const adminRoute = express.Router()
 
 const materialAdminController = require('../controllers/admin/materialAdminController')
+const departmentAdminController =  require('../controllers/admin/departmentAdminController')
 
 adminRoute.get('/materials', [auth, requireStaffOrAdmin], materialAdminController.listMaterials)
 adminRoute.patch('/materials/:materialId/approve', [auth, requireStaffOrAdmin], materialAdminController.approveMaterial)
 adminRoute.patch('/materials/:materialId/disprove', [auth, requireStaffOrAdmin], materialAdminController.disproveMaterial)
 adminRoute.delete('/materials/:materialId', [auth, requireStaffOrAdmin], materialAdminController.deleteMaterial)
+
+
+adminRoute.get('/departments/:departmentId', [auth, requireStaffOrAdmin], departmentAdminController.getDepartment)
+adminRoute.post('/departments', [auth, requireStaffOrAdmin], departmentAdminController.createDeapartment)
+adminRoute.patch('/departments/:departmentId', [auth, requireStaffOrAdmin], departmentAdminController.updateDepartment)
 module.exports = adminRoute
