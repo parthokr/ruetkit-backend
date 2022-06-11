d6801ab7d5cba29def9f04e6882f45f4d2642ace
parthokr <partho.rajvor2016@gmail.com>
2022-06-11 15:07:29 +0600

    Moved department admin route

--- routes/department.route.js
+++ routes/department.route.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const auth = require('../middlewares/auth')
-const requireStaffOrAdmin = require('../middlewares/requireStaffOrAdmin')
+
 const departmentRoute = express.Router()
 
 const departmentController = require('../controllers/departmentController')
 
 departmentRoute.get('/', [auth], departmentController.listAllDepartments)
 departmentRoute.get('/:departmentId/courses', departmentController.listCoursesOfADepartment)
-departmentRoute.post('/', [auth, requireStaffOrAdmin], departmentController.createDeapartment)
 
 module.exports = departmentRoute
