c05bc0c7cbf1a0e9e381d3e176f19abc026c06ce
parthokr <partho.rajvor2016@gmail.com>
2022-06-21 00:14:36 +0600

    Initial commit

---
+++ controllers/notificationController.js
@@ -0,0 +1,41 @@
+const { prisma } = require("@prisma/client")
+const RuetkitError = require("../errors/ruetkit")
+const { 
+    countNotifications: _countNotifications,
+    listNotification: _listNotification,
+    markAsRead: _markAsRead,
+    markAsUnread: _markAsUnread,
+    createNotification: _createNotification
+} = require("../services/manageNotification")
+
+exports.countNotifications = async (req, res, next) => {
+    try {
+        _countNotifications({userID: req.user.id, cb: (code, data) => {res.status(code).send(data)}})
+    } catch (err) {
+        console.log(err)
+        return next(new RuetkitError())
+    }
+}
+
+exports.listNotifications = async (req, res, next) => {
+    _listNotification({userID: req.user.id, cb: (data) => {res.status(200).send(data)}})
+}
+
+exports.markAsRead = async (req, res, next) => {
+    const {notificationID} = req.params
+    _markAsRead({userID: req.user.id, notificationID: notificationID, cb: (code, data) => {res.status(code).send(data)}})
+}
+
+exports.markAsUnread = async (req, res, next) => {
+    const {notificationID} = req.params
+    // _markAsUnread({userID: req.user.id, notificationID: notificastionID, cb: (code, data) => {res.status(code).send(data)}})
+    _markAsUnread({userID: req.user.id, notificationID, cb: (code, data) => {res.status(code).send(data)}})
+}
+
+
+// TODO remove
+exports.createNotification = async (req, res, next) => {
+    const {title, body, navigate} = req.body
+    _createNotification({userID: req.user.id, title, body, severity: "info", navigate, cb: (code, data) => {res.status(code).send(data)}})
+    res.sendStatus(200)
+}
