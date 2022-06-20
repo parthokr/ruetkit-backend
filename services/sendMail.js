9d8b5875ed19e3ba564fba7a300bb790b1eebd7b
parthokr <partho.rajvor2016@gmail.com>
2022-06-21 00:18:37 +0600

    Sending email only in production environment

--- services/sendMail.js
+++ services/sendMail.js
@@ -14,14 +14,23 @@ const transport = nodemailer.createTransport(nodeMailgun(auth))
 
 
 exports.sendMail = ({to, subject, text}) => {
-    const mailOptions = {
-        from: 'RuetKit <noreply@mail.ruetkit.live>',
-        to,
-        subject,
-        html: text
+    // Skip sending email in DEV environment
+    if (process.env.ENVIRONMENT === 'DEV') {
+        console.log('Skipping sending email in development environment');
+        return
+    } 
+    try {
+        const mailOptions = {
+            from: 'RuetKit <noreply@mail.ruetkit.live>',
+            to,
+            subject,
+            html: text
+        }
+        transport.sendMail(mailOptions, (err, data) => {
+        console.log(err);
+        console.log(data);
+    })
+    } catch (err) {
+        throw err
     }
-    transport.sendMail(mailOptions, (err, data) => {
-    console.log(err);
-    console.log(data);
-})
 }
